a7f6fe028aba17e193024148b0c96d355ed96cfa
Emma Butler <emmamac4@gmail.com>
2018-07-20 18:44:03 +0000

    final project

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,42 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do
-    return erb :index
+ get '/' do
+    erb :index
   end
+  
+  post '/' do
+    @first_response = params [:response1]
+    @second_response = params [:response1]
+    @third_response = params [:response1]
+    @result =   kwiz_return(@first_response, @second_response, @third_response)
+    return erb :results
+  end
+  
+  get '/environmentalism' do
+    erb :environmentalism
+  end
+  
+  get '/guncontrol' do
+    erb :guncontrol
+  end
+  
+
+  get '/immigration' do
+    erb :immigration
+  end
+  
+  get '/LGBTQplus' do
+    erb :LGBTQplus
+  end
+  
+   get '/sexism' do
+    erb :sexism
+  end
+  
+   get '/suicideawareness' do
+    erb :suicideawareness
+  end
+  
+
 end
